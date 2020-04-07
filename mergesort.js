f4b08e76e90ccf3fae2192130d4ecd051b54c02e
Adriana Winkelman <adriana.winkelman@gmail.com>
2020-04-07 16:16:02 -0400

    mergeSort in progress

--- mergesort.js
+++ mergesort.js
@@ -1,36 +1,41 @@
-function split (arr){
-    let firstHalf = []
-    let secondHalf = []
+function split(arr) {
+  let firstHalf = [];
+  let secondHalf = [];
 
-    let len = Math.floor(arr.length/2)
+  let len = Math.floor(arr.length / 2);
 
-    firstHalf = arr.slice(0, len)
-    secondHalf = arr.slice(len)
+  firstHalf = arr.slice(0, len);
+  secondHalf = arr.slice(len);
 
-    return [firstHalf, secondHalf]
+  return [firstHalf, secondHalf];
 }
 
-
-function merge (arr1, arr2) {
-    return arr1.concat(arr2)
+function merge(arr1, arr2) {
+  return arr1.concat(arr2);
 }
 
 //[2, 4, 5, 7, 1] === [[2], [4], [5], [7], [1]]
-function mergeSort (arr) {
+function mergeSort(arr) {
+  let result = [];
+  let other;
+  ///base case
+  if (arr.length === 1 || arr.length === 0) {
+    return arr;
+  }
 
-    let result = []
-    ///base case
-    if(arr.length === 1 || arr.length === 0){
-        return arr
-    }
+  for (let i = 0; i < arr.length; i++) {
+    let e = arr[i];
 
-    for (let i = 0; i < arr.length; i++){
-        let e = arr[i]
-        if(Array.isArray(e)){
-            result.push(e)
-        }else{
-            result.push([e])
-        }
+    if (Array.isArray(e)) {
+      if (e.length > 1) {
+        other = mergeSort(e);
+        console.log(other);
+      } else {
+        result.push(e);
+      }
+    } else {
+      result.push([e]);
     }
-    return result
+  }
+  return result.concat(other);
 }
