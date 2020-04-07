ba5ee0da0c86f17b429982b2acfa7592d9e86b9b
Arielle Domantay <domantayarielle@gmail.com>
2020-04-07 15:29:08 -0400

    WIP mergeSort

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,3 +4,9 @@ describe('Split Array function', function() {
       expect(split([2, 4, 5, 7, 1]).length).toEqual(2)
     });
   });
+
+  describe('Merge function', function(){
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      // test the merging algorithm
+    });
+  });
